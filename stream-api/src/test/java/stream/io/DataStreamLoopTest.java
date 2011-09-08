273f086cf874d4f96c9d5a9e646192ad06622719
Christian Bockermann <chris@jwall.org>
2011-09-08 17:11:25 +0200

    Fixed JUnit test and corrected JUnit dependency in stream-mining

--- stream-api/src/test/java/stream/io/DataStreamLoopTest.java
+++ stream-api/src/test/java/stream/io/DataStreamLoopTest.java
@@ -3,38 +3,44 @@ package stream.io;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import stream.data.Data;
 import stream.data.DataImpl;
 
 public class DataStreamLoopTest {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
-		
-		List<Data> items = new ArrayList<Data>();
-		for( int i = 0; i < 10; i++ ){
-			Data item = new DataImpl();
-			item.put( "@id", i + "" );
-			item.put( "x", Math.random() );
-			items.add( item );
-		}
-		
-		DataStreamLoop loop = new DataStreamLoop();
-		loop.setSource( new ListDataStream( items ) );
-		loop.setRepeat( 2 );
-		loop.setShuffle( true );
-		
-		List<Data> result = new ArrayList<Data>();
-		Data datum = loop.readNext();
-		while( datum != null ){
-			result.add( datum );
-			System.out.println( datum );
-			datum = loop.readNext();
-		}
-		
-		System.out.println( "loop resulted in " + result.size() + " items..." );
-	}
+    static Logger log = LoggerFactory.getLogger( DataStreamLoopTest.class );
+    
+    
+    @Test
+    public void testReplication() throws Exception {
+
+        List<Data> items = new ArrayList<Data>();
+        for( int i = 0; i < 10; i++ ){
+            Data item = new DataImpl();
+            item.put( "@id", i + "" );
+            item.put( "x", Math.random() );
+            items.add( item );
+        }
+
+        DataStreamLoop loop = new DataStreamLoop();
+        loop.setSource( new ListDataStream( items ) );
+        loop.setRepeat( 2 );
+        loop.setShuffle( true );
+
+        List<Data> result = new ArrayList<Data>();
+        Data datum = loop.readNext();
+        while( datum != null ){
+            result.add( datum );
+            log.info( "{}", datum );
+            datum = loop.readNext();
+        }
+
+        log.info( "loop resulted in {} items", result.size() );
+        org.junit.Assert.assertTrue( 30 == result.size() );
+    }
 
 }
